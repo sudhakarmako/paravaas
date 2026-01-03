05a630324d172068768db9e13e9a8b4740cd2ff2
sudhakarmako <sudhakar@makoitlab.com>
2026-01-03 13:11:48 +0530

    chore: update drizzle configuration and schema management

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./src/drizzle/migrations",
-  schema: "./src/drizzle/schema",
+  schema: "./src/drizzle/schema.ts",
   dialect: "sqlite",
   dbCredentials: {
     url: process.env.DB_FILE_NAME!,
